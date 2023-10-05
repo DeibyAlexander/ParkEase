733679f556905bfbabe4f66ee3043a9b0383f83e
DeibyAlexander <deiby2707@gmail.com>
2023-10-05 02:28:18 -0500

    feat: :zap: Avances front Sh

--- Frontend/parkease/src/App.js
+++ Frontend/parkease/src/App.js
@@ -1,15 +1,16 @@
 import logo from './assets/img/bggdccfa.png';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="contenedor-global">
+    <Router className="contenedor-global">
       <div className="contenedor">
         <img className="logo" src={logo} alt="si" ></img>
         <a href="/">Iniciar</a>
       </div>
-
-    </div>
+      
+    </Router>
   );
 }
 
